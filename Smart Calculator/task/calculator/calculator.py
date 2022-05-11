b7340dea3d914883c00fdb96ac7a11451fa1599b
rolmos14 <olmos.rafa@gmail.com>
2022-05-11 21:45:03 +0200

    Stage 1/7. Initial version with the class SmartCalculator structure and only addition operation.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,1 +1,27 @@
-# write your code here
+class SmartCalculator:
+
+    operations = ('*', '/', '+', '-')
+
+    def __init__(self):
+        self.result = 0
+
+    def multiplication(self):
+        pass
+
+    def division(self):
+        pass
+
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self):
+        pass
+
+    def run(self):
+        operator_1, operator_2 = [int(op) for op in input().split()]
+        self.result = self.addition(operator_1, operator_2)
+        print(self.result)
+
+
+calculator = SmartCalculator()
+calculator.run()
