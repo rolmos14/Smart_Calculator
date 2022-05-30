95429c0d0478b344177b3ba9abfa67b43305af5f
rolmos14 <olmos.rafa@gmail.com>
2022-05-30 23:11:30 +0200

    Stage 4/7. Addes substraction and operator_sign

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -11,11 +11,20 @@ class SmartCalculator:
     def division(self):
         pass
 
-    def addition(self, nums):
-        return sum(nums)
+    def addition(self, a, b):
+        return a + b
 
-    def subtraction(self):
-        pass
+    def subtraction(self, a, b):
+        return a - b
+
+    def operator_sign(self, operator):
+        if '+' in operator:
+            return '+'
+        if '-' in operator:
+            if len(operator) % 2 != 0:
+                return '-'
+            else:
+                return '+'
 
     def run(self):
         while True:
@@ -24,15 +33,20 @@ class SmartCalculator:
                 continue
             elif len(user_input) == 1:
                 if user_input[0] == "/help":
-                    print("The program calculates the sum of numbers")
+                    print("The program calculates expressions with '+' and '-'")
                 elif user_input[0] == "/exit":
                     print("Bye!")
                     break
                 else:
                     print(*user_input)
             else:
-                operators = [int(op) for op in user_input]
-                self.result = self.addition(operators)
+                self.result = int(user_input[0])
+                for idx in range(1, len(user_input), 2):
+                    op_sign = self.operator_sign(user_input[idx])
+                    if op_sign == '+':
+                        self.result = self.addition(self.result, int(user_input[idx + 1]))
+                    elif op_sign == '-':
+                        self.result = self.subtraction(self.result, int(user_input[idx + 1]))
                 print(self.result)
 
 
