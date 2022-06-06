b84308055e756775b9eda22750f13c6489f191a0
rolmos14 <olmos.rafa@gmail.com>
2022-06-06 23:21:00 +0200

    Stage 5/7. New method expression_valid to validate expression before calculation. run method updated for new cases.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -26,19 +26,45 @@ class SmartCalculator:
             else:
                 return '+'
 
+    def expression_valid(self, expression):
+        # It's a command
+        if expression[0].startswith("/"):
+            return True
+        # Last operator must be just a number, without any operation after
+        try:
+            num = int(expression[-1])
+        except ValueError:
+            return False
+        # Check if there are consecutive operators without operation in between
+        if len(expression) > 1:
+            for op in expression[1::2]:
+                if self.operator_sign(op) not in self.operations:
+                    return False
+        # Valid if no invalid conditions found
+        return True
+
     def run(self):
         while True:
             user_input = input().split()
+            # Input empty
             if not len(user_input):
                 continue
-            elif len(user_input) == 1:
+            if not self.expression_valid(user_input):
+                print("Invalid expression")
+                continue
+            # Length 1 expression
+            if len(user_input) == 1:
                 if user_input[0] == "/help":
                     print("The program calculates expressions with '+' and '-'")
                 elif user_input[0] == "/exit":
                     print("Bye!")
                     break
+                elif user_input[0].startswith("/"):
+                    print("Unknown command")
                 else:
-                    print(*user_input)
+                    # Only one number as input
+                    print(int(user_input[0]))
+            # Length >1 expression
             else:
                 self.result = int(user_input[0])
                 for idx in range(1, len(user_input), 2):
